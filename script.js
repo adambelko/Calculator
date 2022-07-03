f4f877de666e8e55efd5020e845dc46cd1a4872d
Adzi <belkoadam@gmail.com>
2022-07-03 19:31:59 +1000

    display works, sum function works and return the result after pressing equals button

--- script.js
+++ script.js
@@ -2,42 +2,68 @@ const calcNumber = document.querySelectorAll(".number");
 const calcOperator = document.querySelectorAll(".operator");
 const calcDecimal = document.querySelector(".decimal");
 const calcEquals = document.querySelector(".equals");
+const screenTop = document.querySelector(".screenTop");
 const screenBottom = document.querySelector(".screenBottom");
 
-const displayValue = document.createElement("span");
-screenBottom.appendChild(displayValue);
+const displayTop = document.createElement("span");
+const displayBottom = document.createElement("span");
+screenTop.appendChild(displayTop);
+screenBottom.appendChild(displayBottom);
 
 let operator;
 let number;
+let numberTwo;
+let num1;
+let num2;
 let input = [];
+let inputTwo = [];
 
-function getOperator() {
-    for (const x of calcOperator) {
-        x.addEventListener("click", e => {
-            operator = e.currentTarget.id // targeting "id" of a clicked element(operator)
-            console.log(operator);
-        })
-    }
-}
 
 function getNumber() {
     for (const x of calcNumber) {
         x.addEventListener("click", e => {
-            number = e.currentTarget.id
+            number = e.currentTarget.id         // targeting "id" of a clicked element("number-*")
             number = number.replace(/\D/g, ""); //extracting text from my "id" to get number only
             input.push(number);
-            displayValue.textContent = `${input.join("")}`;
-            console.log(input);
+            displayBottom.textContent = `${input.join("")}`;
         });
     }
 }
 
-function sum(a, b) {
-    const result = a + b;
-    console.log(result);
+function getSecondNumber() {
+    for (const x of calcNumber) {
+        x.addEventListener("click", e => {
+            numberTwo = e.currentTarget.id         
+            numberTwo = numberTwo.replace(/\D/g, "");
+            inputTwo.push(numberTwo);
+            num2 = Number(inputTwo.join(""));
+            displayBottom.textContent = `${inputTwo.join("")}`;
+            displayTop.textContent = `${num1}` + " " + `${operator}` + " " + `${num2}`;
+            calcEquals.addEventListener("click", e => operate(operator, num1, num2))
+        });
+    }
 }
 
-function subtract(a, b) {
+function getOperator() {
+    for (const x of calcOperator) {
+        x.addEventListener("click", e => {
+            if (e.currentTarget.id === "add") operator = "+";
+            num1 = Number(input.join(""));
+            displayTop.textContent = `${num1}` + " " + `${operator}` + " "; 
+            displayBottom.textContent = `${num1}`;
+            getSecondNumber()
+        })
+    }
+}
+
+
+
+function sum(a, b) {
+    const result = a + b;
+    displayBottom.textContent = `${result}`;
+}
+
+function subtract(a, b) { 
     const result = a - b;
     return result;
 }
@@ -54,8 +80,8 @@ function divide(a, b) {
 
 
 function operate(operator, num1, num2) {
-    if (operator === "add") sum(num1, num2);
+    if (operator === "+") sum(num1, num2);
 }
 
-getOperator();
 getNumber();
+getOperator();
