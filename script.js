607ce931c9281264115ea4299b98716af98f3cf2
Adzi <belkoadam@gmail.com>
2022-07-07 20:27:09 +1000

    clearNumber andd deleteNumber functions implemented to work with CLEAR and DELETE buttons

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const calcDecimal = document.querySelector(".decimal");
 const calcEquals = document.querySelector(".equals");
 const screenTop = document.querySelector(".screenTop");
 const screenBottom = document.querySelector(".screenBottom");
+const clearAll = document.querySelector(".clear");
+const deleteNum = document.querySelector(".delete");
 
 const displayTop = document.createElement("span");
 const displayBottom = document.createElement("span");
@@ -12,8 +14,8 @@ screenBottom.appendChild(displayBottom);
 
 for (x of calcNumber) {
     x.addEventListener("click", e => {
-        showNumber(e.target.innerText)
-        getNumber(e.target.innerText)
+        showNumber(e.target.innerText);
+        getNumber(e.target.innerText);
     });
 }
 
@@ -22,26 +24,29 @@ for (x of calcOperator){
 }
 
 calcEquals.addEventListener("click", operate);
+clearAll.addEventListener("click", clearDisplay);
+deleteNum.addEventListener("click", deleteNumber);
 
 let num1;
 let num2;
 let currentInput = [];
 let resetDisplay = false;
-let operator
+let operator = null;
 
 function getNumber(currentNumber) {
     currentInput.push(currentNumber);           
 } 
 
 function showNumber(number) {
-    if (resetDisplay === true) displayBottom.textContent = "";
+    if (resetDisplay === true) resetDisplayFn();
     displayBottom.textContent += `${number}`; 
 }
 
 function getOperator(currentOperator) {
+    if (operator !== null) return operate();
     operator = currentOperator;
     num1 = Number(currentInput.join(""));
-    console.log(num1)
+    console.log(num1);
     console.log(currentOperator);
     while (currentInput.length) currentInput.pop();
     displayTop.textContent = `${num1}` + " " + `${operator}` + " "; 
@@ -50,9 +55,11 @@ function getOperator(currentOperator) {
 
 function operate() {
     num2 = Number(currentInput.join(""));
-    console.log(num2)
+    console.log(num2);
     displayTop.textContent = `${num1}` + " " + `${operator}` + " " + `${num2}`;
-    displayBottom.textContent = processCalculation()
+    displayBottom.textContent = processCalculation();
+    num1 = processCalculation();
+    operator = null;
 }
  
 function processCalculation() {
@@ -76,4 +83,23 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+function clearDisplay() {
+    displayTop.textContent = "";
+    displayBottom.textContent = "";
+    operator = null;
+    while (currentInput.length) currentInput.pop();
+    if (num1) while (num1.length) num1.pop();
+    if (num2) while (num2.length) num2.pop();
+}
+
+function deleteNumber() {
+    currentInput.pop();
+    displayBottom.textContent = currentInput;
+}
+
+function resetDisplayFn() {
+    displayBottom.textContent = "";
+    resetDisplay = false;
 }
