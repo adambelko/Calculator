a641a9ecdff2c9c793cd88e910811a47e19e9bc4
Adzi <belkoadam@gmail.com>
2022-08-05 19:40:03 +1000

    Code clean up decimal point and rounding numbers added

--- script.js
+++ script.js
@@ -17,16 +17,20 @@ equalsBtn.addEventListener("click", operate);
 clearBtn.addEventListener("click", clearDisplay);
 deleteBtn.addEventListener("click", deleteLastNumber);
 document.addEventListener("keydown", e => keyboardListener(e));
+decimalBtn.addEventListener("click", e => {
+    addNumber(e.target.innerText);
+    addDecimal(e.target.innerText);
+});
 
 for (let x of classNumber) {
     x.addEventListener("click", e => {
         showNumber(e.target.innerText);
-        saveNumber(e.target.innerText);
+        addNumber(e.target.innerText);
     });
 }
 
 for (let x of classOperator){
-    x.addEventListener("click", e => getOperator(e.target.innerText));
+    x.addEventListener("click", e => addOperator(e.target.innerText));
 }
 
 let num1;
@@ -37,14 +41,18 @@ let operator = null;
 
 function showNumber(number) {
     if (resetBottomDisplay === true) resetDisplay();
-    displayBottom.textContent += `${number}`; 
+    displayBottom.textContent += number;
 }
 
-function saveNumber(selectedNumber) {
+function addNumber(selectedNumber) {
     input.push(selectedNumber);
 } 
 
-function getOperator(selectedOperator) {
+function addDecimal(decimal) {
+    displayBottom.textContent += decimal; 
+}
+
+function addOperator(selectedOperator) {
     if (operator !== null ) operate();
     if (num1 === undefined) num1 = Number(input.join(""));
     operator = selectedOperator;
@@ -53,8 +61,8 @@ function getOperator(selectedOperator) {
     resetBottomDisplay = true;
 }
 
-function operate() {           
-    num2 = Number(input.join(""));
+function operate() {
+    (input.length === 0) ? num2 = num1 : num2 = Number(input.join(""));        // condition if equals button is pressed before second number is declared
     displayTop.textContent = calculate(operator, num1, num2);
     displayBottom.textContent = calculate(operator, num1, num2);
     num1 = calculate(operator, num1, num2);
@@ -70,19 +78,31 @@ function calculate(operator, num1, num2) {
 }
 
 function sum(a, b) {
-    return a + b;
+    const result =  a + b;
+    const roundResult = +parseFloat(result).toFixed(3);
+    return roundResult;
 }
 
 function subtract(a, b) { 
-    return a - b;
+    const result =  a - b;
+    const roundResult = +parseFloat(result).toFixed(3);     // "+" here is removing any zeros that are not needed
+    return roundResult;
 }
 
 function multiply(a, b) {
-    return a * b;
+    const result =  a * b;
+    const roundResult = +parseFloat(result).toFixed(3);
+    return roundResult;
 }
 
 function divide(a, b) {
-    return a / b;
+    if (b === 0) {
+        displayTop.textContent = `Can't divide by "0"`
+    } else {
+        const result =  a / b;
+        const roundResult = +parseFloat(result).toFixed(3);
+        return roundResult;
+    }
 }
 
 function clearDisplay() {
@@ -108,13 +128,16 @@ function resetDisplay() {
 function keyboardListener(e) {
     if (e.key >= 0 && e.key <= 9) {
         showNumber(e.key);
-        saveNumber(e.key);
+        addNumber(e.key);
     }
-    if (e.key === "*") getOperator("x");
-    if (e.key === "/") getOperator("÷");
-    if (e.key === "+" || e.key === "-") getOperator(e.key);
+    if (e.key === "*") addOperator("x");
+    if (e.key === "/") addOperator("÷");
+    if (e.key === "+" || e.key === "-") addOperator(e.key);
     if (e.key === "Enter" || e.key === "=") operate();
-    // if (e.key === ".") getOperator(e.key);
     if (e.key === "Backspace" || e.key === "Delete") deleteLastNumber();
     if (e.key === "Escape") clearDisplay();
+    if (e.key === ".") {
+        showNumber(e.key);
+        addNumber(e.key);
+    }
 }
